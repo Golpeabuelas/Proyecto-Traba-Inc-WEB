8fefeb13a5fc6c7f68f32237a3650d4555cc7a12
Julian <chavez.garcia.julian2@gmail.com>
2024-11-23 00:33:36 -0600

    Corrección para compartir

--- src/server_functions/connection_sql.js
+++ src/server_functions/connection_sql.js
@@ -3,7 +3,7 @@ import mysql from "mysql2"
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Bajoterra_2',
+    password: 'root',
     database: 'TrabaInc'
 })
 
