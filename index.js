5ec8da7ce556a0125a03d8a3b87c4ad6a1e76ede
skwinkle <fernandez.rojas.axelgiovanni@gmail.com>
2024-12-03 01:55:25 -0600

    Pagina principal con los filtros de reciente, cercano y tipos de reporte

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(router)
 app.use(sesiones)
 
 app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, './src/public/html/registrarse.html'))
+    res.sendFile(join(__dirname, './src/public/html/paginaprincipal.html'))
 });
 
 server.listen(app.get('port'), () => {
