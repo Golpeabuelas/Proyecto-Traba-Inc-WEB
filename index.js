81e1a1a4256a8d9b9158464199df609bbc30c8b0
skwinkle <fernandez.rojas.axelgiovanni@gmail.com>
2024-12-03 12:03:56 -0600

    Pagina editar mascota (sin modales xd)

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(router)
 app.use(sesiones)
 
 app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, './src/public/html/hacerReporte.html'))
+    res.sendFile(join(__dirname, './src/public/html/editarmascotas.html'))
 });
 
 server.listen(app.get('port'), () => {
